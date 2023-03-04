c09474ec25aae49bfe18d729a62776f3d67f99b2
Purnama13751 <113276969+Purnama13751@users.noreply.github.com>
2023-03-04 15:17:47 +0700

    Add files via upload

---
+++ Perkenalan.java
@@ -0,0 +1,8 @@
+public class Perkenalan{
+    public static void main(String[] args){
+        String nama = "Muhammad Purnama Putra Pratama";
+        System.out.println("Halo, nama saya adalah "+nama);
+        System.out.println("Saya adalah mahasiswa Teknik Informatika Dian Nuswantoro");
+        System.out.println("Saya sedang menulis program Java");
+    }
+}
---
+++ Segitiga.java
@@ -0,0 +1,9 @@
+public class Segitiga {
+    public static void main(String[] args){
+        int a = 6, t = 8;
+        double luas;
+
+        luas = 0.5*a*t;
+        System.out.print("Luas Segitiga siku-siku adalah "+luas);
+    }
+}
